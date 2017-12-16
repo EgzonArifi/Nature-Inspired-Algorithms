dcfb681ca58c3ece12e5df2452be8b2d015fbc76
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2017-12-16 11:00:03 +0100

    update views, added some

---
+++ Algoritme/IETimetabling.swift
@@ -0,0 +1,18 @@
+//
+//  IETimetabling.swift
+//  Algoritme
+//
+//  Created by Egzon Arifi on 12/8/17.
+//  Copyright © 2017 Overjump. All rights reserved.
+//
+
+import Cocoa
+
+class IETimetabling: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ Algoritme/Timetabling.swift
@@ -0,0 +1,9 @@
+//
+//  Timetabling.swift
+//  Algoritme
+//
+//  Created by Egzon Arifi on 12/8/17.
+//  Copyright © 2017 Overjump. All rights reserved.
+//
+
+import Foundation
