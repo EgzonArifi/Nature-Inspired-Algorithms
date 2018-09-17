2c986743a522c99cdcf8b76095547702091c1b31
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2018-09-17 09:47:35 +0200

    implemented distribution counting sort

--- Advanced Algorithms/DistributionCountingSort.playground/Contents.swift
+++ Advanced Algorithms/DistributionCountingSort.playground/Contents.swift
@@ -2,3 +2,32 @@
 
 import UIKit
 import Foundation
+
+let n = 6
+
+func distributionCountingSort(array: [Int]) -> [Int] {
+    
+    var s = [Int](repeating: 0, count: n)
+    var count = [Int](repeating: 0, count: n)
+    
+    for i in 0..<n-1 {
+        
+        for j in i+1..<n {
+            if array[i] < array[j] {
+                count[j] = count[j] + 1
+            } else {
+                count[i] = count[i] + 1
+            }
+        }
+    }
+    
+    for i in 0..<n {
+        s[count[i]] = array[i]
+    }
+    
+    return s
+}
+
+let array = [13,6,9,3,10,7]
+let sorted = distributionCountingSort(array: array)
+print(sorted)
